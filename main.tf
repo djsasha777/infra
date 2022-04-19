8a5aedb0e081095490842e0d44434292fae885c0
alex <x>
2022-04-20 08:16:04 +1000

    up

--- main.tf
+++ main.tf
@@ -57,4 +57,45 @@ resource "proxmox_vm_qemu" "node" {
   EOF
 }
 
+resource "proxmox_vm_qemu" "lb" {
+  count = 2
+  name = "lb-0${count.index + 1}"
+  target_node = "proxmox"
+
+  clone = "template"
+
+  agent = 1
+  os_type = "cloud-init"
+  cores = 1
+  sockets = 1
+  cpu = "host"
+  memory = 512
+  scsihw = "virtio-scsi-pci"
+  bootdisk = "scsi0"
+
+  disk {
+    slot = 0
+    size = "5"
+    type = "scsi"
+    storage = "drive"
+    iothread = 1
+  }
+
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  ipconfig0 = "ip=192.168.1.22${count.index + 1}/24,gw=192.168.1.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
 
