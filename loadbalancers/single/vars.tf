c012273c5e4d43b38608c65371fcafe41783d74c
alex <x>
2022-05-12 09:48:30 +1000

    up

---
+++ loadbalancers/single/vars.tf
@@ -0,0 +1,11 @@
+variable "ssh_key" {
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCoqvBYIfz/p3cmIDyZCSmggtud2VhT2kWqTh1iooNqF4Ih3CcuhNvWPS/yFCBZuY20z+ZUxrnXin4yAJRs4gXLTeqnkymdUonrPDonsdK2invd+93j02to5TxLR13gPoxRGtPqkfJUBqJ0W7S1oO5ioujJFcYXnXy/GViyunRfbwpxO814fc9CpG2wweHeTaNYvqbTfaUED9ppwDwU+rI+dLnr6xHTx5dI2gE2Y4cPsk+dWEugBFN5Y3uY2McsgD2EWocT4os/QCWY8lJaj6UEpwpDZ6SxYWDnDruBUTnsCbVOGA+q0uaMQ1jOUUijSHJTH0SU+mpA2s+H3zAgeMuQxRfmwCHI1PHO+WAR64+eGhLs4W8WRZxdGMW0q3nx53xaPbP6+A5ZgIR6HRfwJuSrGOsqCg7LcEIc386URf2H8kDhIM0bFVcVfzSTlELBvXFL8RTN6JhhRSvGRByIv+MDB8ldTZRidx6sAtRSv4gqWBgzKSSuSvSfvstctJj0GF0= root@ubunt20"
+}
+
+variable "proxmox_host" {
+    default = "proxmox"
+}
+
+variable "template_name" {
+    default = "template"
+}
