343cb99175ba4ae7712bc08a767612b19d11ecf1
Alex <djsasha777@mail.ru>
2022-08-06 19:06:18 +0300

    up

--- bash/prometheus/install-grafana-ubuntu-apt.sh
+++ bash/prometheus/install-grafana-ubuntu-apt.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y software-properties-common wget
 sudo wget -q -O /usr/share/keyrings/grafana.key https://packages.grafana.com/gpg.key
 echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt-get update
-sudo apt-get install grafana-enterprise
+sudo apt-get install -y grafana-enterprise
 sudo systemctl daemon-reload
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server.service
