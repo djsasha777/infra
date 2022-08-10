75605cfe933387b7881e342f54b2bb1fbf1af582
Alex <djsasha777@mail.ru>
2022-08-10 12:31:27 +0300

    up

--- bash/elk/install-filebeat.sh
+++ bash/elk/install-filebeat.sh
@@ -7,35 +7,9 @@ sudo apt-get install -y filebeat
 elif [[ "$VERS" == 'centos' ]]; then
 echo "your distributive is ${VERS}"
 yum install filebeat
-cat >> /etc/filebeat/filebeat.yml <<EOF
-filebeat.inputs:
-- type: log
-  enabled: true
-  paths:
-      - /var/log/nginx/*-access.log
-  fields:
-    type: nginx_access
-  fields_under_root: true
-  scan_frequency: 5s
-
-- type: log
-  enabled: true
-  paths:
-      - /var/log/nginx/*-error.log
-  fields:
-    type: nginx_error
-  fields_under_root: true
-  scan_frequency: 5s
-
-output.logstash:
-  hosts: ["192.168.1.98:5044"]
-
-xpack.monitoring:
-  enabled: true
-  elasticsearch:
-    hosts: ["http://192.168.1.98:9200"]
 EOF
 else
   echo "DISTRIBUTIVE NOT SUPPORT!"
 fi
 echo "Filebeat are installed!"
+cp filebeat.yml /etc/filebeat/
