2d6f7c61ce4dab2532938406e3226784a7f85689
Alex <djsasha777@mail.ru>
2022-08-14 12:52:22 +0300

    up

--- terraform/kubernetes-on-proxmox/main.tf
+++ terraform/kubernetes-on-proxmox/main.tf
@@ -16,8 +16,12 @@ provider "proxmox" {
 }
 
 resource "proxmox_vm_qemu" "node" {
-  count = 5
-  name = "node-0${count.index + 1}"
+  timeouts {
+    create = "20m"
+    delete = "2h"
+  }
+  count = 4
+  name = "kubernetes-${count.index + 1}"
   target_node = "proxmox"
   
   clone = "centos"
@@ -56,4 +60,47 @@ resource "proxmox_vm_qemu" "node" {
   EOF
 }
 
+resource "proxmox_vm_qemu" "nodeadd" {
+  timeouts {
+    create = "10m"
+    delete = "2h"
+  }
+  count = 1
+  name = "kubernetes-5"
+  target_node = "proxmox"
+  
+  clone = "centos"
 
+  agent = 1
+  os_type = "cloud-init"
+  cores = 2
+  sockets = 1
+  vcpus = 0
+  cpu = "host"
+  memory = 6144
+  scsihw = "lsi"
+  bootdisk = "scsi0"
+
+  disk {
+    size            = "60G"
+    type            = "scsi"
+    storage         = "drive"
+  }
+
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+  
+
+  lifecycle {
+    ignore_changes = [
+      network,
+    ]
+  }
+
+  ipconfig0 = "ip=192.168.1.205/24,gw=192.168.1.1"
+  sshkeys = <<EOF
+  ${var.ssh_key}
+  EOF
+}
