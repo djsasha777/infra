fcda010fa2c5d32c544fbcde7215b6f0903f9683
Alex <djsasha777@mail.ru>
2022-08-19 14:56:06 +0300

    up

--- bash/elk/install-filebeat.sh
+++ bash/elk/install-filebeat.sh
@@ -2,13 +2,14 @@
 VERS=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
 if [[ "$VERS" == 'ubuntu' ]]; then
 echo "your distributive is ${VERS}"
-apt update
-sudo apt-get install -y filebeat
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.4.1-amd64.deb
+sudo dpkg -i filebeat-7.4.1-amd64.deb
 elif [[ "$VERS" == 'centos' ]]; then
 echo "your distributive is ${VERS}"
-yum install filebeat
+curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.4.1-x86_64.rpm
+sudo rpm -vi filebeat-7.4.1-x86_64.rpm
 else
   echo "DISTRIBUTIVE NOT SUPPORT!"
 fi
 echo "Filebeat are installed!"
-cp filebeat.yml /etc/filebeat/
+cp filebeat.yml /etc/filebeat/filebeat.yml
