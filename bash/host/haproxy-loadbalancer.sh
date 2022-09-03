b477dfeafecb2f168f3562acf4e2b9b6213d8d97
Alex <djsasha777@mail.ru>
2022-09-03 19:42:04 +0300

    up

--- bash/host/haproxy-loadbalancer.sh
+++ bash/host/haproxy-loadbalancer.sh
@@ -11,15 +11,17 @@ else
   echo "DISTRIBUTIVE NOT SUPPORT!"
 fi
 echo "HAproxy server software is installed! now configuring!"
-echo "
-  listen kubernetes-apiserver-https
-  bind *:8383
-  mode tcp
-  option log-health-checks
-  timeout client 3h
-  timeout server 3h
-  server master1 192.168.1.111:6443 check check-ssl verify none inter 10000
-  server master2 192.168.1.112:6443 check check-ssl verify none inter 10000
-  balance roundrobin" >> /etc/haproxy/haproxy.cfg
+cat  >> /etc/haproxy/haproxy.cfg <<EOF
+
+listen kubernetes-apiserver-https
+ bind *:8383
+ mode tcp
+ option log-health-checks
+ timeout client 3h
+ timeout server 3h
+ server master1 192.168.1.111:6443 check check-ssl verify none inter 10000
+ server master2 192.168.1.112:6443 check check-ssl verify none inter 10000
+ balance roundrobin
+EOF
 sudo systemctl restart haproxy  
 echo "DONE"
--- bash/host/zerotier.sh
+++ bash/host/zerotier.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+VERS=$(awk '/^ID=/' /etc/*-release | awk -F'=' '{ print tolower($2) }')
+if [[ "$VERS" == 'ubuntu' ]]; then
+echo "your distributive is ${VERS}"
+sudo apt update
+sudo apt instll -y curl gpg
+elif [[ "$VERS" == 'centos' ]]; then
+echo "your distributive is ${VERS}"
+yum install curl gpg
+else
+  echo "DISTRIBUTIVE NOT SUPPORT!"
+fi
 echo "This script will install and connect zerotier to your system"
 echo "please enter zerotier netwirk id"
 NETID=$(read)
