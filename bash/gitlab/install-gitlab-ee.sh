d676c7d2a550ea3f5e15a964b08f65594b2cde1b
Alex <djsasha777@mail.ru>
2023-02-09 18:05:34 +0300

    up

--- bash/gitlab/install-gitlab-ee.sh
+++ bash/gitlab/install-gitlab-ee.sh
@@ -17,6 +17,13 @@ elif [ "$(grep -Ei 'fedora|redhat|centos' /etc/*release)" ]
     then
         echo "It's a RedHat based system."
         dnf install -y nano curl openssh-server ca-certificates tzdata perl postfix
+        systemctl enable sshd
+        systemctl start sshd
+        firewall-cmd --permanent --add-service=http
+        firewall-cmd --permanent --add-service=https
+        systemctl reload firewalld
+        systemctl enable postfix
+        systemctl start postfix
         wget ftp://192.168.1.1/AiDisk_a1/repository/gitlab-ee_15.6.7-ee.0_amd64.deb
         EXTERNAL_URL="https://172.22.22.115" dpkg -i gitlab-ee_15.6.7-ee.0_amd64.deb
         dnf -i gitlab-ee_15.6.7-ee.0_amd64.deb
