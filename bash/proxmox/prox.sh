80beb79a546294c1f91bd1918a806d5113cddb6e
Alex <64518378+djsasha777@users.noreply.github.com>
2023-08-20 09:49:03 +0300

    Update prox.sh

--- bash/proxmox/prox.sh
+++ bash/proxmox/prox.sh
@@ -1,4 +1,14 @@
+
+
+# for erasing proxmox drive
 lvremove /dev/pve/data  
 
+# convert disc to qcow2
+qemu-img convert -O qcow2 /dev/SSD-860/vm-109-disk-0 /mnt/openwrt.qcow2
 
-for erasing proxmox drive
+# impord disc
+qm importdisk 700 /mnt/openwrt.qcow2 SSD860 -format raw
+
+
+#add phisical disc to vm
+qm set 700 -scsi1 /dev/disk/by-id/ata-HP_SSD_S750_1TB_HASA43140200190
