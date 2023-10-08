66e28197cf45054a6947df2ce6f3b79eb8bf7647
Alex <64518378+djsasha777@users.noreply.github.com>
2023-10-08 11:30:56 +0300

    Update prox.sh

--- bash/proxmox/prox.sh
+++ bash/proxmox/prox.sh
@@ -12,3 +12,19 @@ qm importdisk 700 /mnt/openwrt.qcow2 SSD860 -format raw
 
 #add phisical disc to vm
 qm set 700 -scsi1 /dev/disk/by-id/ata-HP_SSD_S750_1TB_HASA43140200190
+
+#add ntfs drive to proxmox ct
+#connect drive
+mkdir /media/ntfs
+apt update
+apt install ntfs-3g
+#add to /etc/fstab
+nano /etc/fstab
+#add this->            /dev/sda1   /media/ntfs ntfs-3g    permissions,locale=en_US.utf8    0   2
+systemctl daemon-reload
+mount -a
+#turn on CT and create dir on it ->     mkdir /ntfs
+#stop ct
+nano /etc/pve/lxc/200.conf
+#add this ->          mp1: /media/ntfs,mp=/ntfs
+#start ct
