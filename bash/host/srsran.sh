d38299612b6839f03e39eef7ab68fe71a7ea453e
Alex <64518378+djsasha777@users.noreply.github.com>
2024-09-24 19:32:15 +0300

    Update srsran.sh

--- bash/host/srsran.sh
+++ bash/host/srsran.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt update && apt install cmake build-essential libfftw3-dev libmbedtls-dev libboost-program-options-dev libconfig++-dev libsctp-dev libzmq3-dev git
+apt update && apt install cmake make gcc g++ pkg-config build-essential libfftw3-dev libmbedtls-dev libyaml-cpp-dev libboost-program-options-dev libconfig++-dev libgtest-dev libsctp-dev libzmq3-dev git
 git clone https://github.com/srsRAN/srsRAN_4G.git
 cd srsRAN_4G
 mkdir build
