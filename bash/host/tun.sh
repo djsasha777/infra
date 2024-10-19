39bc18d150ad1f8615c221d10cf51ef2901689db
Alex <64518378+djsasha777@users.noreply.github.com>
2024-10-19 21:09:50 +0300

    Create tun.sh

---
+++ bash/host/tun.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+cat  >> /etc/systemd/system/tun2socks <<EOF
+[Unit]
+Description=Tun2Socks
+After=network.target
+
+[Service]
+Type=simple
+User=root
+EnvironmentFile=/etc/tun2socks/tun2socks.conf
+ExecStartPre=-ip tuntap add mode tun dev tun0
+ExecStartPre=ip addr add ${TUNIP}/${TUNPREFIX} dev tun0
+ExecStartPre=ip link set dev tun0 up
+ExecStart=tun2socks -device tun://tun0 -proxy ss://${SSPROTOCOL}:${SSPASSWORD}@${SSIP}:${SSPORT}
+ExecStartPost=ip r add default dev tun0 metric 50
+ExecStopPost=-ip r flush table lip
+ExecStopPost=-ip rule delete table lip
+ExecStopPost=-ip link set dev tun0 down
+ExecStopPost=-ip link del dev tun0
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+mkdir /etc/tun2socks
+cat  >> /etc/tun2socks/tun2socks.conf <<EOF
+SSPROTOCOL=chacha20-ietf-poly1305
+SSIP=195.0.0.1
+SSPORT=8443
+SSPASSWORD=YourPassword
+EOF
+
+systemctl enable --now tun2socks
