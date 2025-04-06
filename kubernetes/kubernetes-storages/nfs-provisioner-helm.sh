bb38929eb02c6b8dc970c0631ff445c33202f0a1
Alex <djsasha777@mail.ru>
2025-04-06 18:42:34 +0300

    clean

--- kubernetes/kubernetes-storages/nfs-provisioner-helm.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner --set nfs.server=192.168.1.1 --set nfs.path=/media/AiDisk_a1/nfs
