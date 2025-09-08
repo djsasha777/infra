248e98d9a4eaa5c74b59ac1c930931cd48216ff2
Alex <djsasha777@mail.ru>
2025-09-08 18:09:33 +0300

    add adderoll

---
+++ bash/adderoll/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Запрос переменных у пользователя
+read -p "Введите name: " name
+read -p "Введите ip: " ip
+read -p "Введите port: " port
+
+file="val.yaml"
+
+# Проверка, существует ли запись с таким name
+exists=$(yq e ".acmeSubdomain[] | select(.name == \"$name\")" $file)
+
+if [ -n "$exists" ]; then
+  # Если существует, заменяем запись с именем $name на новые значения
+  yq e -i "(.acmeSubdomain[] | select(.name == \"$name\")) |= {name: \"$name\", ip: \"$ip\", port: $port}" $file
+  echo "Запись с name='$name' обновлена."
+else
+  # Если не существует, добавляем новую запись в список
+  yq e -i ".acmeSubdomain += [{name: \"$name\", ip: \"$ip\", port: $port}]" $file
+  echo "Новая запись с name='$name' добавлена."
+fi
